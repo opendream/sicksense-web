892a0e6946e9f49eb923f5146bc16014daddcacc
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2014-06-02 22:22:23 +0700

    Add week selector UI

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 $(document).foundation();
 
 $(document).ready(function() {
-  var map = L.map('map').setView([13.751690, 100.491882], 13);
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
-
   var knot = $('.knots-ui');
 
   $('.knot-pin').click(function(e) {
@@ -12,12 +9,56 @@ $(document).ready(function() {
   });
 });
 
-function getWeeks(year, quarter) {
+function Dashboard () {
+  var API_BASEPATH = "http://localhost:1337";
+
+  this.map = null;
+  this.markers = [];
+
+  this.initMap = function(options) {
+    options = _.defaults(options || {}, {
+      latitude: 13.751690,
+      longitude: 100.491882,
+      zoom: 13
+    });
+
+    this.map = L.map('map').setView([options.latitude, options.longitude], options.zoom);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(this.map);
+
+    return this.map;
+  };
+
+  this.clearMarkers = function() {
+    var self = this;
+    _.each(self.markers, function(item) {
+      self.map.removeLayer(item);
+    });
+  };
+
+  this.addMarker = function(latitude, longitude) {
+    this.markers.push( L.marker([latitude, longitude]).addTo(this.map) );
+  };
+
+  this.refresh = function(city, weekDate) {
+    var self = this;
+
+    self.clearMarkers();
+
+    $.getJSON(API_BASEPATH + '/dashboard?callback=?', { city: city, date: (new Date(weekDate)).toISOString() })
+      .done(function(data) {
+        _.each(data.response.reports.items, function(item) {
+          self.addMarker(item.latitude, item.longitude);
+        });
+      });
+  };
+}
+
+function getWeeks (year, quarter) {
   // 1. Get first day of quarter
   var firstDateOfQuarter = new Date(moment(year + '-01-01').quarter(quarter));
 
   // 2. Get first day of that week
-  var firstDateOfTheWeek = moment(new Date(firstDateOfQuarter)).subtract('day', firstDateOfQuarter.getDay());
+  var firstDateOfTheWeek = moment(new Date(firstDateOfQuarter)).startOf('week');
   // 3. Generate each first day of week by adding 7 days until the last week before next quarter
   var weeks = [ new Date(firstDateOfTheWeek) ];
   var run = true;
@@ -25,7 +66,7 @@ function getWeeks(year, quarter) {
 
   while (run) {
     tmp = moment(new Date(_.last(weeks))).add('days', 7);
-    if (tmp.quarter() > quarter) {
+    if (tmp.year() > year || tmp.quarter() > quarter) {
       run = false;
     }
     else {
@@ -36,16 +77,34 @@ function getWeeks(year, quarter) {
   return weeks;
 }
 
-function WeekSelector($scope) {
+function getWeekDate (dateInWeek) {
+  var date = new Date(dateInWeek || new Date());
+  return new Date(moment(date).startOf('week'));
+}
+
+function WeekSelector ($scope) {
+  // Init map
+  var dashboard = new Dashboard();
+  dashboard.initMap();
+
   moment.lang('en');
 
+  $scope.city = "Bangkok";
   $scope.year = moment().year();
   $scope.quarter = moment().quarter();
+
+  $scope.$watch('weekDate', function(newValue, oldValue) {
+    dashboard.refresh($scope.city, newValue);
+  });
+  $scope.weekDate = getWeekDate();
+
   $scope.years = [ 2014, 2013 ];
 
   $scope.monthNames = moment.months();
 
   $scope.weeks = getWeeks($scope.year, $scope.quarter);
+
+
 
   $scope.setYear = function(value) {
     Foundation.libs.dropdown.closeall();
@@ -58,6 +117,15 @@ function WeekSelector($scope) {
   };
 
   $scope.setWeek = function(weekDate) {
+    $scope.weekDate = new Date(weekDate);
+  };
 
+  $scope.isSelectedWeek = function(date) {
+    return (new Date(date)).getTime() === (new Date($scope.weekDate)).getTime();
+  };
+
+  $scope.quarterExpand = false;
+  $scope.toggleQuartersUI = function() {
+    $scope.quarterExpand = !$scope.quarterExpand;
   };
 }
