d2c94d839c7c95c5f52c60df5469702b87e1446b
Nirut Khemasakchai <rutcreate@gmail.com>
2014-07-30 13:43:09 +0700

    - Refactor code. - Delete item.

--- js/admin.js
+++ js/admin.js
@@ -1,42 +1,58 @@
 (function ($, window, document, undefined) {
   var url = API_BASEPATH + '/notifications/';
+  var minAge = 1;
+  var maxAge = 130;
+  var processing = false;
 
   var app = angular.module('sicksense', []);
 
   app.controller('NotificationList', ['$scope', function($scope) {
 
+    /**
+     * Items to display.
+     */
     $scope.items = [];
 
-    // Gender.
+    /**
+     * Gender options.
+     */
     $scope.genderOptions = {
       'all': 'ทุกเพศ',
       'male': 'ชาย',
       'female': 'หญิง'
     };
 
-    // Province.
-    $scope.provinceOptions = {'-': '- เลือกจังหวัด ',"Bangkok":"กรุงเทพมหานคร","Bueng Kan":"บึงกาฬ","Samut Prakarn":"สมุทรปราการ","Nonthaburi":"นนทบุรี","Pathumthani":"ปทุมธานี","Phra Nakhon Sri Ayutthaya":"พระนครศรีอยุธยา","Angthong":"อ่างทอง","Lopburi":"ลพบุรี","Singburi":"สิงห์บุรี","Chainat":"ชัยนาท","Saraburi":"สระบุรี","Chonburi":"ชลบุรี","Rayong":"ระยอง","Chanthaburi":"จันทบุรี","Trat":"ตราด","Chachoengsao":"ฉะเชิงเทรา","Prachinburi":"ปราจีนบุรี","Nakhon Nayok":"นครนายก","Srakaeo":"สระแก้ว","Nakhon Ratchasima":"นครราชสีมา","Buri Ram":"บุรีรัมย์","Surin":"สุรินทร์","Si Sa Ket":"ศรีสะเกษ","Ubon Ratchathani":"อุบลราชธานี","Yasothon":"ยโสธร","Chaiyaphum":"ชัยภูมิ","Amnat Charoen":"อำนาจเจริญ","Nong Bua Lam Phu":"หนองบัวลำภู","Khon Kaen":"ขอนแก่น","Udon Thani":"อุดรธานี","Loei":"เลย","Nongkai":"หนองคาย","Maha Sarakham":"มหาสารคาม","Roi Et":"ร้อยเอ็ด","Kalasin":"กาฬสินธุ์","Sakon Nakhon":"สกลนคร","Nakhon Phanom":"นครพนม","Mukdahan":"มุกดาหาร","Chiang Mai":"เชียงใหม่","Lamphun":"ลำพูน","Lamphang":"ลำปาง","Uttaradit":"อุตรดิตถ์","Phrae":"แพร่","Nan":"น่าน","Phayao":"พะเยา","Chiang Rai":"เชียงราย","Mae Hong Son":"แม่ฮ่องสอน","Nakhon Sawan":"นครสวรรค์","Uthai Thani":"อุทัยธานี","Kamphaeng Phet":"กำแพงเพชร","Tak":"ตาก","Sukhothai":"สุโขทัย","Phitsanulok":"พิษณุโลก","Phichit":"พิจิตร","Phetchabun":"เพชรบูรณ์","Ratchburi":"ราชบุรี","Kanchanaburi":"กาญจนบุรี","Supanburi":"สุพรรณบุรี","Nakhon Pathom":"นครปฐม","Samut Sakhon":"สมุทรสาคร","Samut Songkram":"สมุทรสงคราม","Petchburi":"เพชรบุรี","Phangnga":"พังงา","Prachaubkirikhan":"ประจวบคีรีขันธ์","Nakhon Sri Thammarat":"นครศรีธรรมราช","Krabi":"กระบี่","Phuket":"ภูเก็ต","Surat Thani":"สุราษฎร์ธานี","Ranong":"ระนอง","Chumphon":"ชุมพร","Songkhla":"สงขลา","Satun":"สตูล","Trang":"ตรัง","Phatthalung":"พัทลุง","Pattani":"ปัตตานี","Yala":"ยะลา","Narathiwat":"นราธิวาส"};
+    /**
+     * Province options.
+     */
+    $scope.provinceOptions = {'-': 'ทุกจังหวัด ',"Bangkok":"กรุงเทพมหานคร","Bueng Kan":"บึงกาฬ","Samut Prakarn":"สมุทรปราการ","Nonthaburi":"นนทบุรี","Pathumthani":"ปทุมธานี","Phra Nakhon Sri Ayutthaya":"พระนครศรีอยุธยา","Angthong":"อ่างทอง","Lopburi":"ลพบุรี","Singburi":"สิงห์บุรี","Chainat":"ชัยนาท","Saraburi":"สระบุรี","Chonburi":"ชลบุรี","Rayong":"ระยอง","Chanthaburi":"จันทบุรี","Trat":"ตราด","Chachoengsao":"ฉะเชิงเทรา","Prachinburi":"ปราจีนบุรี","Nakhon Nayok":"นครนายก","Srakaeo":"สระแก้ว","Nakhon Ratchasima":"นครราชสีมา","Buri Ram":"บุรีรัมย์","Surin":"สุรินทร์","Si Sa Ket":"ศรีสะเกษ","Ubon Ratchathani":"อุบลราชธานี","Yasothon":"ยโสธร","Chaiyaphum":"ชัยภูมิ","Amnat Charoen":"อำนาจเจริญ","Nong Bua Lam Phu":"หนองบัวลำภู","Khon Kaen":"ขอนแก่น","Udon Thani":"อุดรธานี","Loei":"เลย","Nongkai":"หนองคาย","Maha Sarakham":"มหาสารคาม","Roi Et":"ร้อยเอ็ด","Kalasin":"กาฬสินธุ์","Sakon Nakhon":"สกลนคร","Nakhon Phanom":"นครพนม","Mukdahan":"มุกดาหาร","Chiang Mai":"เชียงใหม่","Lamphun":"ลำพูน","Lamphang":"ลำปาง","Uttaradit":"อุตรดิตถ์","Phrae":"แพร่","Nan":"น่าน","Phayao":"พะเยา","Chiang Rai":"เชียงราย","Mae Hong Son":"แม่ฮ่องสอน","Nakhon Sawan":"นครสวรรค์","Uthai Thani":"อุทัยธานี","Kamphaeng Phet":"กำแพงเพชร","Tak":"ตาก","Sukhothai":"สุโขทัย","Phitsanulok":"พิษณุโลก","Phichit":"พิจิตร","Phetchabun":"เพชรบูรณ์","Ratchburi":"ราชบุรี","Kanchanaburi":"กาญจนบุรี","Supanburi":"สุพรรณบุรี","Nakhon Pathom":"นครปฐม","Samut Sakhon":"สมุทรสาคร","Samut Songkram":"สมุทรสงคราม","Petchburi":"เพชรบุรี","Phangnga":"พังงา","Prachaubkirikhan":"ประจวบคีรีขันธ์","Nakhon Sri Thammarat":"นครศรีธรรมราช","Krabi":"กระบี่","Phuket":"ภูเก็ต","Surat Thani":"สุราษฎร์ธานี","Ranong":"ระนอง","Chumphon":"ชุมพร","Songkhla":"สงขลา","Satun":"สตูล","Trang":"ตรัง","Phatthalung":"พัทลุง","Pattani":"ปัตตานี","Yala":"ยะลา","Narathiwat":"นราธิวาส"};
 
-    $scope.reset = function() {
+    /**
+     * Reset add form.
+     */
+    $scope.resetForm = function() {
       $scope.body = '';
       $scope.gender = 'all';
       $scope.published = moment().format('YYYY-MM-DDT08:00:00') + 'Z';
       $scope.province = '-';
-      $scope.age_start = 20;
-      $scope.age_stop = 40;
+      $scope.age_start = minAge;
+      $scope.age_stop = maxAge;
     };
 
-    $scope.load = function() {
+    /**
+     * Load items.
+     */
+    $scope.loadItems = function() {
       $.getJSON(url)
         .done(function(resp) {
           $scope.$apply(function() {
             resp.response.notifications.items.forEach(function(notification) {
               $scope.items.push(notification);
-            })
+            });
           });
         })
         .error(function(resp) {
-
+          alert('Error: ' + resp.responseJSON.meta.errorDetail);
         });
     };
 
@@ -44,7 +60,10 @@
 
     };
 
-    $scope.add = function() {
+    /**
+     * Add new item.
+     */
+    $scope.addItem = function() {
       $scope.validate();
 
       var params = {
@@ -56,81 +75,139 @@
         age_stop: $scope.age_stop
       };
 
+      processing = true;
+      $('#delete-item-button').prop('disabled', true);
+      $('#delete-cancel-button').prop('disabled', true);
+
       $.post(url, params)
         .done(function(resp) {
           $scope.$apply(function() {
             $scope.items.unshift(resp.response.notification);
           });
+
+          $scope.cancelModal('#add-new');
         })
         .error(function(resp) {
-
+          alert('Error: ' + resp.responseJSON.meta.errorDetail);
+          processing = false;
+          $scope.cancelModal('#add-new');
         });
-
-      $scope.close('#add-new');
     };
 
-    $scope.delete = function() {
+    /**
+     * Show confirm delete dialog.
+     */
+    $scope.showConfirmDeleteDialog = function() {
       $scope.current = this.item;
       $('#confirm-delete').foundation('reveal', 'open');
     };
 
-    $scope.confirm_delete = function() {
-      $scope.items.forEach(function(item, idx) {
-        if (item.id == $scope.current.id) {
-          $scope.items.splice(idx, 1);
-        }
-      });
-      $scope.close('#confirm-delete');
+    /**
+     * Delete item.
+     */
+    $scope.deleteItem = function() {
+      var deleteURL = url + $scope.current.id + '/delete/';
+      $.post(deleteURL)
+        .done(function(resp) {
+          $scope.$apply(function() {
+            $scope.items.forEach(function(item, idx) {
+              if (item.id == $scope.current.id) {
+                item.status = 3;
+              }
+            });
+            $scope.closeModal('#confirm-delete');
+          });
+        })
+        .error(function(resp) {
+          alert('Error: ' + resp.responseJSON.meta.errorDetail);
+          $scope.closeModal('#confirm-delete');
+        });
+
     };
 
-    $scope.cancel = function(selector) {
-      $scope.close(selector);
-      $scope.reset();
+    /**
+     * Cancel button action.
+     */
+    $scope.cancelModal = function(selector) {
+      $scope.closeModal(selector);
+      $scope.resetForm();
     };
 
-    $scope.close = function(selector) {
+    /**
+     * Close reveal modal by CSS selector.
+     */
+    $scope.closeModal = function(selector) {
       $(selector).foundation('reveal', 'close');
+      processing = false;
+      $('#add-item-button').prop('disabled', false);
+      $('#add-cancel-button').prop('disabled', false);
+      $('#delete-item-button').prop('disabled', false);
+      $('#delete-cancel-button').prop('disabled', false);
     };
 
+    /**
+     * Return province as Thai.
+     */
     $scope.getProvinceText = function(province) {
       return $scope.provinceOptions[province];
     };
 
+    /**
+     * Return gender code as text.
+     */
     $scope.getGenderText = function(gender) {
       return $scope.genderOptions[gender];
     };
 
-    $scope.reset();
-    $scope.load();
+    /**
+     * Return date formatted.
+     */
+    $scope.getDateFormatted = function(date) {
+      return moment(date).format('D MMM YYYY - HH:mm:ss');
+    }
 
-    // Init date picker.
-    $('.date-picker').dtpicker({
-      calendarMouseScroll: false,
-      minuteInterval: 15,
-      current: $scope.published,
-      timelistScroll: false,
-      onHide: function(handler) {
-        $scope.$apply(function() {
-          var dt = moment(handler.getDate());
-          $scope.published = dt.format('YYYY-MM-DDTHH:mm:ss') + 'Z';
-        });
-      }
-    });
+    /**
+     * Initialize app.
+     */
+    $scope.init = function() {
+      // Set default values to add form.
+      $scope.resetForm();
 
-    // Init slider.
-    $('#slider-range').slider({
-      range: true,
-      min: 1,
-      max: 125,
-      values: [ $scope.age_start, $scope.age_stop ],
-      slide: function( event, ui ) {
-        $scope.$apply(function() {
-          var values = ui.values;
-          $scope.age_start = values[0];
-          $scope.age_stop = values[1];
-        });
-      }
-    });
+      // Start load items.
+      $scope.loadItems();
+
+      // Init date picker.
+      $('.date-picker').dtpicker({
+        calendarMouseScroll: false,
+        minuteInterval: 15,
+        current: $scope.published,
+        timelistScroll: false,
+        onHide: function(handler) {
+          $scope.$apply(function() {
+            var dt = moment(handler.getDate());
+            $scope.published = dt.format('YYYY-MM-DDTHH:mm:ss') + 'Z';
+          });
+        }
+      });
+
+      // Init slider.
+      $('#slider-range').slider({
+        range: true,
+        min: minAge,
+        max: maxAge,
+        values: [ $scope.age_start, $scope.age_stop ],
+        slide: function( event, ui ) {
+          $scope.$apply(function() {
+            var values = ui.values;
+            $scope.age_start = values[0];
+            $scope.age_stop = values[1];
+          });
+        }
+      });
+    };
+
+    // Start
+    $scope.init();
 
   }]);
 
