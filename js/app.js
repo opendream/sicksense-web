e4d6036466c33b4e2a9f58ae5650858db62715e3
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2014-08-08 11:51:47 +0700

    Remove jReject, use static page instead

--- js/app.js
+++ js/app.js
@@ -11,23 +11,3 @@ var app = angular.module('sicksense', []);
 app.factory('shared', function() {
     return {};
 });
-
-
-/**
- * Browser warning.
- */
-jQuery(document).ready(function ($) {
-    $.reject({
-        reject: {
-            all: false,
-            msie: 9
-        },
-        header: 'เว็บเบราเซอร์ของคุณล้าสมัยแล้ว',
-        paragraph1: 'คุณกำลังใช้เบราเซอร์ที่เราไม่ได้รองรับ',
-        paragraph2: 'โปรดติดตั้งเบราเซอร์ที่เราสนับสนุนตามที่แสดงข้างล่างนี้เพื่อใช้งานเว็บได้อย่างถูกต้อง',
-        close: false,
-        closeLink: 'ปิดหน้าต่างนี้',
-        closeMessage: 'ปิดหน้าต่างนี้และยอมรับความเสี่ยงของการแสดงผลที่ไม่ถูกต้อง',
-        imagePath: '/bower_components/jReject/images/'
-    });
-});
